9bba1be8ba224e02e2871260ae1a07453a64c89d
DjapelZed <maks.kushnirov@gmail.com>
2016-08-30 20:30:46 +0300

    Add position

---
+++ SnakeApp/SnakeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char symb1 = '@';
+            SymbPos(x1, y1, symb1);
+        }
+
+        static void SymbPos(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+}
